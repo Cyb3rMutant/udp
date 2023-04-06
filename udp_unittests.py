b82bcc9f2cf134fdfbeaf874106d492ee0cf64b8
yazeed <yazeed2.abu-hummos@live.uwe.ac.uk>
2023-04-07 00:54:22 +0100

    unittests did not await

--- udp_unittests.py
+++ udp_unittests.py
@@ -6,27 +6,37 @@ from datetime import datetime
 
 class TestSum(unittest.TestCase):
     def test_001_checksum_1_2_AB(self):
-        udp_server.compute_checksum(1, 2, b"AB")
+        checksum = udp_server.compute_checksum(1, 2, b"AB")
+
+        self.assertEqual(checksum, 45501)
 
     def test_002_checksum_0_0_(self):
-        udp_server.compute_checksum(0, 0, b"")
+        checksum = udp_server.compute_checksum(0, 0, b"")
+
+        self.assertEqual(checksum, 63487)
 
     def test_003_checksum_0_542_1111(self):
-        udp_server.compute_checksum(0, 542, b"1111")
+        checksum = udp_server.compute_checksum(0, 542, b"1111")
+        
+        self.assertEqual(checksum, 29595)
 
+    def test_004_checksum_0_0_INVALID_CHECKSUM_SEND(self):
+        checksum = udp_server.compute_checksum(0, 0, b"INVALID CHECKSUM SEND")
+
+        self.assertEqual(checksum, 9207)
 
 class TestAsync(unittest.IsolatedAsyncioTestCase):
     async def test_004_morse_server_echo(self):
         async with websockets.connect("ws://localhost:5612") as websocket:
-            payload = udp_server.recv_and_decode_packet(websocket)
+            payload = await udp_server.recv_and_decode_packet(websocket)
 
             self.assertEqual(payload, "Welcome to IoT UDP Server")
 
     async def test_005_morse_server_time(self):
         async with websockets.connect("ws://localhost:5612") as websocket:
-            websocket.recv()
-            udp_server.send_packet(websocket, 0, 542, b'1111')
-            payload = udp_server.recv_and_decode_packet(websocket)
+            await websocket.recv()
+            await udp_server.send_packet(websocket, 0, 542, b'1111')
+            payload = await udp_server.recv_and_decode_packet(websocket)
 
             self.assertEqual(payload, datetime.now().strftime("%H:%M:%S"))
 
